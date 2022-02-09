4d0c38091909f0595c26993413fd6aef9d6a2c6f
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 08:43:56 +0500

    Initial Commit

---
+++ Yan_SwiftUI_State_Toggle/Yan_SwiftUI_State_ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_State_ToggleApp.swift
+//  Yan_SwiftUI_State_Toggle
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_State_ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
